9f5edb65c57087b7856c929ff80bb61e777c41f1
huseyin <huseyintasdemir74@hotmail.com>
2025-05-23 21:09:46 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -91,7 +91,7 @@ resource "aws_instance" "kube-master" {
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
   subnet_id = "subnet-09e3a026f5de5ecce"  # select own subnet_id of us-east-1a
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "kube-master"
     Project = "tera-kube-ans"
@@ -107,7 +107,7 @@ resource "aws_instance" "worker-1" {
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
   subnet_id = "subnet-09e3a026f5de5ecce"  # select own subnet_id of us-east-1a
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "worker-1"
     Project = "tera-kube-ans"
@@ -123,7 +123,7 @@ resource "aws_instance" "worker-2" {
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
   key_name = "clarus"
   subnet_id = "subnet-09e3a026f5de5ecce"  # select own subnet_id of us-east-1a
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "worker-2"
     Project = "tera-kube-ans"
